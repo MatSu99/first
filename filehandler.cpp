c710d9b638bc200e2bd6ed0cf89864bb2a951962
MatSu99 <109213225+MatSu99@users.noreply.github.com>
2022-07-16 17:53:27 +0200

    File handler development
    Important try throw catch decision and counting number of lines
    in file.

--- filehandler.cpp
+++ filehandler.cpp
@@ -2,12 +2,53 @@
 
 FileHandler::FileHandler() {
     Name = "logs.txt";
+    NumberOfLines = 0;
 
+    std::fstream file;
+    file.open(Name, std::ios::in);
+
+    if(file.good()==false) {
+        std::cout << "ERROR, CONSTRUCTOR HAVE NOT FOUND " << Name << " file!";
+        //return "E";
+        throw 100; // To do handling exepctions
+    }
+
+    std::string line;
+
+        while(getline(file,line))
+    {
+        ++NumberOfLines;
+    }
+
+    //++NumberOfLines;
+
+    file.close();
 }
 
-FileHandler::FileHandler(std::string In_Name)
+FileHandler::FileHandler(std::string _InName)
 {
-    Name = In_Name;
+    Name = _InName;
+    NumberOfLines = 0;
+
+    std::fstream file;
+    file.open(Name, std::ios::in);
+
+    if(file.good()==false) {
+        std::cout << "ERROR, CONSTRUCTOR HAVE NOT FOUND " << Name << " file!";
+        //return "E";
+        throw 100; // To do handling exepctions
+    }
+
+    std::string line;
+
+    while(getline(file,line))
+    {
+        ++NumberOfLines;
+    }
+
+    //++NumberOfLines;
+
+    file.close();
 }
 
 FileHandler::~FileHandler() {
@@ -22,6 +63,7 @@ std::string FileHandler::GetFromLogs() {
 
     if(file.good()==false) {
         std::cout << "ERROR";
+        throw 101;
         return "E";
     }
 
@@ -42,5 +84,87 @@ std::string FileHandler::GetFromLogs() {
     // TO DO 
 
     return line;
+
+}
+
+std::string FileHandler::GetLineNumber( int _LineNumber) {
+
+    if(_LineNumber > NumberOfLines) {
+        std::cout<< "Number of of range \n";
+        return "XXX";
+    }
+
+    std::fstream file;
+    file.open("logs.txt", std::ios::in);
+
+    if(file.good()==false) {
+        std::cout << "ERROR";
+        throw 101;
+        return "E";
+    }
+
+    std::string line;
+
+    for (int i = 0; i<_LineNumber; i++) {
+
+            getline(file,line);
+
+    }
+
+    file.close();
+
+    return line;
+
+}
+
+void FileHandler::PrintProperties() {
+
+    std::cout << "Name of file: "<< Name << "\n";
+
+    std::cout << "Number of lines: "<< NumberOfLines << "\n";
+
+    return;
+}
+
+void FileHandler::GetLinesToVector(int _LineStart, int _LineStop, std::vector<std::string> &_TargetVectoer) {
+
+    if( _LineStart < 0 || _LineStart > NumberOfLines) {
+        std::cout << "Starting point error\n";
+        return;
+    }
+
+    if(_LineStop < _LineStart || _LineStop > NumberOfLines) {
+        std::cout << "Stoping point error\n";
+        return;
+    }
+
+    std::fstream file;
+    file.open("logs.txt", std::ios::in);
+
+    if(file.good()==false) {
+        std::cout << "ERROR";
+        throw 101;
+        return;
+    }
+
+    std::string line;
+
+    int Marker = 1;
+
+    while(Marker < _LineStart) {
+
+        getline(file,line);
+        ++Marker;
+    }
+
+    while(Marker != _LineStop+1) {
+
+        getline(file,line);
+        _TargetVectoer.push_back(line);
+        ++Marker;
+    }
+
+    file.close();
+
 
 }
--- filehandler.h
+++ filehandler.h
@@ -4,12 +4,16 @@ class FileHandler {
 
     private:
         std::string Name;
+        int NumberOfLines;
 
     public:
     FileHandler();
-    FileHandler(std::string In_Name);
+    FileHandler(std::string _InName);
     ~FileHandler();
     std::string GetFromLogs();
+    std::string GetLineNumber(int _LineNumber);
+    void GetLinesToVector(int _LineStart, int _LineStop, std::vector<std::string> &_TargetVectoer);
 
+    void PrintProperties();
 
 };
